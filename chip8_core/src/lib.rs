787225ebc30757ca6b7a7cdc2aa52ab981859828
cat <kittynekostudio@gmail.com>
2024-10-11 15:14:40 -0700

    Added methods to the Emulation struct.

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -8,6 +8,27 @@ const NUM_REGS: usize = 16;
 const STACK_SIZE: usize = 16;
 const NUM_KEYS: usize = 16;
 
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 pub struct Emulator {
     pc: u16,
     ram: [u8; RAM_SIZE],
@@ -23,7 +44,7 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new() -> Self {
-        Self {
+        let mut new_emulator = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -34,6 +55,51 @@ impl Emulator {
             keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
+        };
+
+        new_emulator.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        new_emulator
+    }
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+    pub fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+    pub fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize]
+    }
+    pub fn tic(&mut self) {
+        let op = self.fetch();
+    }
+    pub fn tic_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
         }
+        if self.st > 0 {
+            if self.st == 1 {
+                // TODO! sould play sound
+            }
+            self.st -= 1;
+        }
+    }
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[self.pc as usize - 1] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
     }
 }
